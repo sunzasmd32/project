e3537cc7d45f7667965f6b09385e8d4c0b819b7b
folk <florkgum@hotmail.com>
2019-06-13 11:35:00 +0700

    UpdateMenuAdmin//UpdateViewBook

--- bookrental/admin/protected/views/book/_view.php
+++ bookrental/admin/protected/views/book/_view.php
@@ -5,25 +5,25 @@
 
 <div class="view">
 
-	<b><?php echo CHtml::encode($data->getAttributeLabel('id_book')); ?>:</b>
-	<?php echo CHtml::link(CHtml::encode($data->id_book), array('view', 'id'=>$data->id_book)); ?>
-	<br />
 
-	<b><?php echo CHtml::encode($data->getAttributeLabel('bookname')); ?>:</b>
+	<b><?php echo CHtml::encode($data->getAttributeLabel('ชื่อหนังสือ :')); ?>:</b>
 	<?php echo CHtml::encode($data->bookname); ?>
 	<br />
 
-	<b><?php echo CHtml::encode($data->getAttributeLabel('price')); ?>:</b>
+	<b><?php echo CHtml::encode($data->getAttributeLabel('ราคา/วัน')); ?>:</b>
 	<?php echo CHtml::encode($data->price); ?>
 	<br />
 
-	<b><?php echo CHtml::encode($data->getAttributeLabel('quantity')); ?>:</b>
+	<b><?php echo CHtml::encode($data->getAttributeLabel('จำนวนหนังสือ')); ?>:</b>
 	<?php echo CHtml::encode($data->quantity); ?>
 	<br />
 
-	<b><?php echo CHtml::encode($data->getAttributeLabel('image')); ?>:</b>
-	<?php echo CHtml::encode($data->image); ?>
-	<br />
+	<div  align="center">
+	<br>
+
+	<?php echo CHtml::image(Yii::app()->baseUrl.'/upload/'.$data->image,'',array('height'=>'250', 'width'=>'250')); ?>
+
+</div>
 
 
 </div>
