18c77ff8b3fc589ed35a572a5d9ce144060f3af1
Nopphakon <44893298+Nopphakon@users.noreply.github.com>
2019-06-13 09:41:08 +0700

    theme

--- bookrental/protected/config/main.php
+++ bookrental/protected/config/main.php
@@ -8,6 +8,7 @@
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
 	'name'=>'Book Rental Store',
+	'theme'=>'blackboot',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
