4c1b6bc5326b4d00b617d0b56ef27f8f5aaeffaa
folk <florkgum@hotmail.com>
2019-06-13 17:44:51 +0700

    UpdateControllerBook(USER)

--- bookrental/protected/views/book/_view.php
+++ bookrental/protected/views/book/_view.php
@@ -2,28 +2,52 @@
 /* @var $this BookController */
 /* @var $data Book */
 ?>
+<form action="" method="post">
 
-<div class="view">
+<div class=	"view">
 
-	<b><?php echo CHtml::encode($data->getAttributeLabel('id_book')); ?>:</b>
-	<?php echo CHtml::link(CHtml::encode($data->id_book), array('view', 'id'=>$data->id_book)); ?>
-	<br />
 
-	<b><?php echo CHtml::encode($data->getAttributeLabel('bookname')); ?>:</b>
+	<?php echo CHtml::image(Yii::app()->baseUrl.'/admin/upload/'.$data->image,'',array('height'=>'250', 'width'=>'250')); ?><br>
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('ชื่อหนังสือ')); ?>:</b>
 	<?php echo CHtml::encode($data->bookname); ?>
 	<br />
 
-	<b><?php echo CHtml::encode($data->getAttributeLabel('price')); ?>:</b>
+	<b><?php echo CHtml::encode($data->getAttributeLabel('ราคา/วัน')); ?>:</b>
 	<?php echo CHtml::encode($data->price); ?>
 	<br />
 
-	<b><?php echo CHtml::encode($data->getAttributeLabel('quantity')); ?>:</b>
+	<b><?php echo CHtml::encode($data->getAttributeLabel('จำนวนคงเหลือ')); ?>:</b>
 	<?php echo CHtml::encode($data->quantity); ?>
 	<br />
 
-	<b><?php echo CHtml::encode($data->getAttributeLabel('image')); ?>:</b>
-	<?php echo CHtml::encode($data->image); ?>
-	<br />
+<input type="text" name="test">
+	<select name="<?php echo($data->id_book) ?>">
+  		<option value="1">1เล่ม</option>
+  		<option value="2">2เล่ม</option>
+  		<option value="3">3เล่ม</option>
+ 		<option value="4">4เล่ม</option>
+ 		<option value="5">5เล่ม</option>
+ 		<option value="6">6เล่ม</option>
+ 		<option value="7">7เล่ม</option>
+	</select>
 
+	<select name="<?php echo($data->id_book) ?>">
+  		<option value="1">1วัน</option>
+  		<option value="2">2วัน</option>
+  		<option value="3">3วัน</option>
+ 		<option value="4">4วัน</option>
+ 		<option value="5">5วัน</option>
+ 		<option value="6">6วัน</option>
+ 		<option value="7">7วัน</option>
+	</select>
+	<input type="submit" name="addcart" value="เพิ่มลงตะกร้า">
+<hr><br>
 
 </div>
+
+
+
+
+
+</form>
